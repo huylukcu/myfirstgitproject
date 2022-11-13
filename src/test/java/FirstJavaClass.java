6b3d56547c3703a6a5b5a62bb56dd9c50b52d00b
hayriye uylukcu <huylukcu@gmail.comgit config --global user.email huylukcu@gmail.comgit config --global user.name hayriye>
2022-11-13 15:46:10 -0600

    integer

--- src/test/java/FirstJavaClass.java
+++ src/test/java/FirstJavaClass.java
@@ -3,5 +3,7 @@ public class FirstJavaClass {
     public static void main(String[] args) {
         System.out.println("Learning GitHub");
         System.out.println("I love GitHub");
+         int a = 5;
+        System.out.println(a);
     }
 }
